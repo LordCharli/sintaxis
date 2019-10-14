7433eecb8b956c117971f8b35d639eec7444571f
LordCharli <charlidaw2@gmail.com>
2019-10-14 17:24:22 +0200

    cambio

---
+++ js/externo.js
@@ -0,0 +1,9 @@
+alert("hola mundo");
+confirm("comfirmacion");
+prompt("ver");
+
+document.write("codigo reescrito");
+console.log("consola");
+
+console.warn("advertencia");
+console.error("error");
