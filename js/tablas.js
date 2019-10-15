2b7185be56c9ddc42bcbf531af5e2b6f4d7a529f
LordCharli <charlidaw2@gmail.com>
2019-10-15 20:14:10 +0200

    ejercicio 4

--- js/tablas.js
+++ js/tablas.js
@@ -3,11 +3,12 @@ n=prompt('Introduce un numero','');
 n=parseInt(n);
 
 
-
-for (let index = 0; index < 10; index++) {
-  document.write(n+" * "+index+" = "+(n*index)+"<br>");
+let tabla = "<table border=1>";
+for (let index = 1; index <= 10; index++) {
+  tabla+="<tr><td>"+n+" * "+index+"</td><td>"+(n*index)+"</td></tr>";
 }
-
+tabla+="</table>";
+document.write(tabla);
 
 
 
